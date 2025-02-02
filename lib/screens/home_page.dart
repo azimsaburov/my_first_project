975b60a82ef6bd0dfe8dab058d567b7b1bd0027f
Azim <azimsab2000@gmail.com>
2025-02-02 16:12:39 +0600

    добавил кнопку и каритнку

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,6 +8,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isImages = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,15 +27,40 @@ class _HomePageState extends State<HomePage> {
                 onPressed: () => Navigator.pop(context),
                 child: Text('Вернуться назад!'),
               ),
-              // GridView.count(
-              //   crossAxisCount: 2, // 2 колонки
-              //   children: [
-              //     Container(color: Colors.red, height: 100),
-              //     Container(color: Colors.blue, height: 100),
-              //     Container(color: Colors.green, height: 100),
-              //     Container(color: Colors.yellow, height: 100),
-              //   ],
-              // ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Container(
+                          color: Colors.amber,
+                          height:
+                              10)), //Expanded заставляет каждый контейнер занять всю доступную ширину, деля её поровну между собой.
+                  Expanded(child: Container(color: Colors.red, height: 10)),
+                  Flexible(
+                    flex: 1, // Этот элемент займет 1 часть пространства
+                    child: Container(color: Colors.red, height: 100),
+                  ),
+                  Flexible(
+                    flex: 2, // Этот элемент займет 2 части пространства
+                    child: Container(color: Colors.green, height: 100),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Image.asset(
+                isImages ? 'assets/images/anime1.jpg' : 'assets/images/d2.jpg',
+                width: 500,
+                height: 500,
+              ),
+              IconButton(
+                icon: Icon(Icons.star, color: Colors.amber),
+                splashRadius: 25,
+                highlightColor: Colors.black,
+                onPressed: () {
+                  setState(() {
+                    isImages = !isImages;
+                  });
+                },
+              ),
             ],
           ),
         ),
