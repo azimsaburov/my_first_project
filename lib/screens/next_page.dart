05b55d1573816d3d17efdef7aa14b95a61d7a07e
Azim <azimsab2000@gmail.com>
2025-02-06 14:31:55 +0600

    Добавил Страницу товаров

--- lib/screens/next_page.dart
+++ lib/screens/next_page.dart
@@ -10,6 +10,22 @@ class NextPage extends StatefulWidget {
 class _NextPageState extends State<NextPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        color: Colors.green,
+        width: 400,
+        height: 200,
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Acer Aspir 5'),
+              subtitle: Text('Описание'),
+              trailing: Icon(Icons.image),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
